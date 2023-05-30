1d3c01db2c83ecbd1b765d281c3eda8c13fbcc79
DrippedOut <86663132+DrippedOut@users.noreply.github.com>
2023-05-30 14:46:02 +0000

    Commit swapping
    main.cpp, makefile & result

--- main.cpp
+++ main.cpp
@@ -1,33 +1,35 @@
 #include <iostream>
 using namespace std;
-//1. Creating make file
-//2. Swap the value in the array using pointers** to help
-//3. Take input from argv
+// 1. Creating make file
+// 2. Swap the value in the array using pointers** to help
+// 3. Take input from argv
+int main(int argc, char** argv) {
+  int n = argc - 1;
+  int* a = new int[n];
 
+  for (int i = 0; i < n; i++) {
+    a[i] = atoi(argv[i + 1]);
+    cout << "a[" << i << "] = " << a[i] << endl;
+  }
 
-int main(int argc, char **argv){
- int i,n,*a;
-			n=argc-1;
- 		     a=new int[n];
-			for(i=0;i<n;i++) {
-			    a[i]=atoi(argv[i]);
-		       cout<< "a[" << i << "]=" <<a[i]<<endl;
-		}
+  int* start = a;
+  int* end = a + n - 1;
 
-    int*p=a;
-    int*pb;
+  // Swapping
+  while (start < end) {
+    int temp = *start;
+    *start = *end;
+    *end = temp;
+    start++;
+    end--;
+  }
 
+  // Print result
+  for (int i = 0; i < n; i++) {
+    cout << a[i] << " ";
+  }
 
-  
-     // Using p within your program 
-  for(i=0;i<n;i++) {
- 		   cout<<p[i]<<" "; //p is the location where p points to
- //     p++;  
- }
+  delete[] a;
 
- delete [] a;  
+  return 0;
 }
-
-  
-//}
-
---
+++ makefile
@@ -0,0 +1,4 @@
+DEPS= result
+compile: main.cpp 
+	 g++ main.cpp -o $(DEPS)
+
